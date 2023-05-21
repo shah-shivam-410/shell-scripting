3394796d07c4ac6b7720d1ba066b702f14303258
shah-shivam-410 <shah.shivam.410@gmail.com>
2023-05-21 18:53:47 +0530

    Completed with tempelate creation

---
+++ scripts/count-cmd-parameters.sh
@@ -0,0 +1,14 @@
+#Description: To count and work with inut parameters provided while running shell scripts.
+#Version: 1.0.0
+#Creation date: Sun, May 21, 2023  6:50:08 PM
+#Author: S S
+
+#START
+
+#!/bin/bash
+echo "Count of input parameters: $#"
+echo "First input parameters: $1"
+echo "Second input parameters: $2"
+echo "Third input parameters: $3"
+
+#END
